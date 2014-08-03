c9d86f33d35b12b6bd87db04fbc48bb6119b39e5
hchtz <ybkk1027@foxmail.com>
2014-08-03 13:56:26 +0800

    Signed-off-by: hchtz <ybkk1027@foxmail.com>

--- assets/images/pages/ygfc/yasuo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-for img in `find ./ -name "*.jpg"`; do
-                convert -resize 15%*15% $img $img-resized;
-                rm $img;
-                mv $img-resized $img
-                echo $img
-done
